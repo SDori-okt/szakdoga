dd3e830d86a2fba81ca085360d3364b32a871821
Stencinger Dóra <dora.stencinger@gmail.hu>
2023-03-02 14:57:08 +0100

    Add index.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -21,6 +21,7 @@
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content flex-col lg:flex-row-reverse">
         <div class="text-center lg:text-left">
+            <img src="../images/logo.svg" alt="ShareOn" title="ShareOn">
             <h1 class="text-5xl font-bold">Login now!</h1>
             <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
         </div>
