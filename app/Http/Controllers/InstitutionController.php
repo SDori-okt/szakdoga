9dd75a8b09fb39d21cb07e72edf21fd46312e555
Stencinger Dóra <dora@ibookr.com>
2023-03-10 10:51:38 +0100

    Get list of institutions

---
+++ app/Http/Controllers/InstitutionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+
+class InstitutionController extends Controller
+{
+    public function getInstitutions()
+    {
+        $apiURL = 'https://kretaglobalmobileapi2.ekreta.hu/api/v3/Institute';
+        $headers = [
+            'apiKey' => '7856d350-1fda-45f5-822d-e1a2f3f1acf0'
+        ];
+
+        $response = Http::withHeaders($headers)->get($apiURL);
+        $response = json_decode($response, true);
+
+        return $response;
+    }
+}
