074b9c82413cd87134d7f1c7096e9fe0ff5e3f8f
Stencinger Dóra <dora@ibookr.com>
2023-04-15 12:43:39 +0200

    Add alerts on success or fail

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -9,7 +10,9 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\File;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Prologue\Alerts\Facades\Alert;
 
 class FileController extends Controller
 {
@@ -20,35 +23,52 @@ class FileController extends Controller
 
     public function store(Request $request): RedirectResponse
     {
-        $file = new File;
+        try {
+            $file = new File;
 
-        $request->validate([
-            'file' => 'required|file|mimes:pdf,doc,docx,xls,xlsx,ppt,pptx|max:1024'
-        ]);
+            $request->validate([
+                'file' => 'required|file|mimes:pdf,doc,docx,xls,xlsx,ppt,pptx|max:1024'
+            ]);
 
-        $fileName = time() . '_' . $request->file('file')->getClientOriginalName();
-        $request->file('file')->storeAs('public/files', $fileName);
+            $fileName = time() . '_' . $request->file('file')->getClientOriginalName();
+            $request->file('file')->storeAs('public/files', $fileName);
 
-        $file->file_name = $fileName;
-        $file->title = $request->input('title');
-        $file->subject = $request->input('subject');
-        $file->topic = $request->input('topic');
-        $file->difficulty_level = $request->input('difficulty_level');
-        $file->type = "röpdolgozat";
-        $file->time = 5;
-        $file->num_of_downloads = 0;
-        $file->user_id = 2;
+            $file->file_name = $fileName;
+            $file->title = $request->input('title');
+            $file->subject = $request->input('subject');
+            $file->topic = $request->input('topic');
+            $file->difficulty_level = $request->input('difficulty_level');
+            $file->type = "röpdolgozat";
+            $file->time = 5;
+            $file->num_of_downloads = 0;
+            $file->user_id = 2;
 
-        $file->save();
+            $file->save();
+            Alert::success('Fájl feltöltése sikeres.');
 
-        return redirect()->route('home')->with('success', 'Fájl feltöltése sikeres.');
+            return redirect()->route('home')->with('success', 'Fájl feltöltése sikeres.');
+        } catch (Exception $e) {
+            Alert::error('Fájl feltöltése sikertelen.');
+            Log::debug($e);
+
+            return redirect()->route('home')->with('error', 'Fájl feltöltése sikertelen.');
+        }
     }
 
     public function destroy(File $file): RedirectResponse
     {
-        $file->delete();
+        try {
+            $file->delete();
+            Alert::success('Fájl törlése sikeres.');
 
-        return redirect()->route('dashboard')->with('success', 'Fájl törlése sikeres.');
+            return redirect()->route('dashboard')->with('success', 'Fájl törlése sikeres.');
+        } catch (Exception $e) {
+            Alert::error('Fájl törlése sikertelen.');
+            Log::debug($e);
+
+            return redirect()->route('home')->with('error', 'Fájl törlése sikertelen.');
+        }
+
     }
 
     public function downloadFile($filename): Response
@@ -60,7 +80,7 @@ class FileController extends Controller
         $file = Storage::disk('public')->get($filename);
         $response = new Response($file, 200);
         $response->header('Content-Type', Storage::disk('public')->mimeType($filename));
-        $response->header('Content-Disposition', 'attachment; filename="'.$filename.'"');
+        $response->header('Content-Disposition', 'attachment; filename="' . $filename . '"');
 
         return $response;
     }
