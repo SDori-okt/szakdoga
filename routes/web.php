6b62e4d59bbbbe211c81676f68addb1dee63eed1
Stencinger Dóra <dora@ibookr.com>
2023-04-17 11:13:47 +0200

    Change routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\InstitutionController;
 use App\Http\Controllers\TypeController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
