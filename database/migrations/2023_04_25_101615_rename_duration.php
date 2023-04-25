e69d745a3677c7fa5f78fd669128e49aec7590cc
Stencinger Dóra <dora@ibookr.com>
2023-04-25 12:38:25 +0200

    Rename database column

--- database/migrations/2023_04_25_101615_rename_duration.php
+++ database/migrations/2023_04_25_101615_rename_duration.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,10 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('files', function (Blueprint $table) {
-            $table->softDeletes();
-        });
-        Schema::table('users', function (Blueprint $table) {
-            $table->softDeletes();
+            $table->renameColumn('time', 'duration');
         });
     }
 
@@ -28,7 +24,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-
+        Schema::table('files', function (Blueprint $table) {
+            $table->renameColumn('duration', 'time');
+        });
     }
 };
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -96,49 +96,6 @@
         </div>
     </div>
 </div>
-<footer class="bg-neutral text-neutral-content">
-    <div class="footer p-10">
-        <div class="grid-cols-1">
-            <img src="images/logo.svg" alt="ShareOn" class="w-24">
-            <p class="w-100">
-                Keress, találj, értékelj, alkoss és ossz meg! Együtt könyebb!
-                <br>
-                Ne többet dolgozz, hanem okosabban!
-            </p>
-        </div>
-        <div class="col-sm-3">
-            <span class="footer-title">Oldaltérkép</span>
-            <div class="grid grid-flow-col gap-4">
-                <ul>
-                    <li><a href="#">Adatkezelési tájékoztató</a></li>
-                    <li><a href="#">Süti nyilatkozat</a></li>
-                    <li><a href="#">Impresszum</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-sm-3">
-            <span class="footer-title">Dokumentumok</span>
-            <div class="grid grid-flow-col gap-4">
-                <ul>
-                    <li><a href="#">Adatkezelési tájékoztató</a></li>
-                    <li><a href="#">Süti nyilatkozat</a></li>
-                    <li><a href="#">Impresszum</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-sm-3">
-            <span class="footer-title">Social</span>
-            <div class="grid grid-flow-col gap-4">
-                <span class="fa fa-facebook"></span>
-                <span class="fa fa-instagram"></span>
-                <span class="fa fa-twitter"></span>
-            </div>
-        </div>
-    </div>
-    <div class="text-sm px-10 text-neutral-content">
-        <p class="py-3 border-t-2 border-white">2023 <span class="fa fa-copyright"></span> ShareOn. Design: Stencinger
-            Dóra</p>
-    </div>
-</footer>
+@include('footer')
 </body>
 </html>
